4cc106caec153536f87138938ef38137324331a3
unknown <rithinja@gmail.com>
2020-01-13 22:08:58 +0530

    first commit

---
+++ js/menu-icon-animate.js
@@ -0,0 +1,5 @@
+const menuOne = document.querySelector('.menuOne');
+function addClassFunOne() {
+       this.classList.toggle("clickMenuOne");
+   }
+    menuOne.addEventListener('click', addClassFunOne);
